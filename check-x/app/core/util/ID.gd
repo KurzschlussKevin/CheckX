7ec352b1778b603dcf917c18fdd313b10084f380
KurzschlussKevin <61833020+KurzschlussKevin@users.noreply.github.com>
2026-02-10 12:32:25 +0100

    Delete check-x directory

--- check-x/app/core/util/ID.gd
+++
@@ -1,8 +0,0 @@
-extends RefCounted
-class_name Id
-
-static func new_id() -> String:
-	# simple unique-ish id without external libs
-	var t := str(Time.get_unix_time_from_system())
-	var r := str(randi() % 1000000).pad_zeros(6)
-	return t + "-" + r
